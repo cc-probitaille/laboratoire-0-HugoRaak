07b05605319decba662a4a5059795a65289e95fb
HugoRaak <rathle.hugo@gmail.com>
2025-09-15 12:38:35 -0400

    fix: display scoreboard

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,7 @@ import logger from 'morgan';
 import flash from 'express-flash-plus';
 
 import { jeuRoutes } from './routes/jeuRouter';
+import { Joueur } from './core/joueur';
 
 // Creates and configures an ExpressJS web server.
 class App {
@@ -61,13 +62,18 @@ class App {
 
     // Route pour classement (stats)
     router.get('/stats', (req, res, next) => {
+      const joueurs: Array<Joueur> = JSON.parse(jeuRoutes.controleurJeu.joueurs);
+      const joueursAvecRatio = joueurs.map(j => ({
+        ...j,
+        ratio: j.lancers === 0 ? 0 : j.lancersGagnes / j.lancers
+      }));
       res.render('stats',
         // passer objet au gabarit (template) Pug
         {
           title: `${titreBase}`,
           user: user,
           // créer nouveau tableau de joueurs qui est trié par ratio
-          joueurs: JSON.parse(jeuRoutes.controleurJeu.joueurs)
+          joueurs: joueursAvecRatio.sort((elA, elB) => elB.ratio - elA.ratio)
         });
     });
 
